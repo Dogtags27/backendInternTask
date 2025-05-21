aade4d001a45d57c35e43040ba1c586ba40df4b7
HridayDesai <hriday.desai@pepsales.ai>
2025-05-21 09:36:39 +0530

    Deployment Bug Fixes

--- run.py
+++ run.py
@@ -1,26 +1,6 @@
-from flask import Flask
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
-from app.routes import identify_bp
+from app import create_app
 
-load_dotenv()
+app = create_app()
 
-app = Flask(__name__)
-app.register_blueprint(identify_bp)
-CORS(app)
-
-# Optional: root route for health check
-@app.route('/')
-def home():
-    return "Backend is running!"
-
-# Load config from config.py or directly from env
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
